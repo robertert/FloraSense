612a27de02128a8b6081bd4f3de8e40ebf751a27
galekkapi <kapigalson@gmail.com>
2025-11-19 22:47:26 +0100

    dodanie dzialajacego mqtt

--- main/flora-sense.c
+++ main/flora-sense.c
@@ -12,14 +12,14 @@
 #include "nvs_flash.h"
 #include "esp_http_client.h"
 #include "esp_netif.h"
-
+#include "mqtt_client.h" 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "driver/gpio.h"
-
+#include "flora_mqtt.h"
 #include "config.h"
 #include "wifi.h"
-#include "itag_client.h"
+//#include "itag_client.h"
 #include "http_client.h"
 
 void nvs_init(void)
@@ -37,9 +37,12 @@ void app_main(void)
 {
     nvs_init();
     wifi_init();
+    mqtt_app_start(); 
     // init_ble();
+    xTaskCreate(mqtt_publish_task, "mqtt_pub_task", 4096, NULL, 5, NULL);  
+
     // Zwiększony stack dla http_get_task - potrzebny dla operacji sieciowych
-    xTaskCreate(&http_get_task_raw, "http_get_task_raw", 8192, NULL, 5, NULL);
+    //xTaskCreate(&http_get_task_raw, "http_get_task_raw", 8192, NULL, 5, NULL);
 
 
 }
